946f0114949098d55e978e615d4977dbadeafe38
Nicholas Alexander Taras <nicholas.taras@nab.com.au>
2018-05-07 22:14:55 +1000

    added graphic for floor and logic to game

---
+++ src/Assets/Scripts/Alien.cs
@@ -0,0 +1,93 @@
+﻿/* Alien (UNITY C#)
+* 
+* Alien:
+* 
+* USE:
+* - To orchestrate the Alien ai.
+* - This class has waypoints and move towards classes.
+* 
+* PARAMS:
+* - tagName of way point to use
+* 
+* TEST: 
+* tbc
+* 
+* AUTHOR: 
+* Nicholas Taras
+*
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien : MonoBehaviour
+{
+    
+	public class AlienManager
+	{
+		public GameObject wayPointTarget;
+		public bool isAllowedToMove;
+
+		public AlienManager(bool _isAllowedToMove)
+		{
+			isAllowedToMove = _isAllowedToMove;
+		}
+
+		public AlienManager()
+		{
+			isAllowedToMove = false;
+		}
+	}
+
+	public AlienManager alien = new AlienManager(false);
+
+	void Start()
+	{
+		alien.isAllowedToMove = true;
+	}
+
+	bool wayPointReached(GameObject _target)
+	{
+
+		Vector3 currentPosition = transform.position;
+
+		bool _wayPointReached = false;
+
+		float distance = (currentPosition - _target.transform.position).sqrMagnitude;
+
+        // Close to Target - Adjust for precision.
+		if (distance < Random.Range(1, 2))
+		{ 
+			_wayPointReached = true;
+		}
+
+		return _wayPointReached;
+
+	}
+
+	void move()
+	{
+
+		// Apply for sequential waypoints.
+		WaypointManager _WayPointManager = GetComponent<WaypointManager>();
+        alien.wayPointTarget = _WayPointManager.getCurrentWayPoint();
+
+		MoveTowards _moveTowards = GetComponent<MoveTowards>();
+        _moveTowards.moveToTarget(alien.wayPointTarget);
+
+	}
+
+	void Update()
+	{
+
+        if (alien.isAllowedToMove)
+		{
+
+			move();
+
+		}
+
+	}
+
+}
---
+++ src/Assets/Scripts/WaypointManager.cs
@@ -0,0 +1,126 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour {
+
+    public string tagName;
+
+	class WayPointController
+	{
+		public GameObject[] objects;
+		public bool[] checkList;
+		public string tagName;
+		public int currentIndex;
+
+		public WayPointController()
+		{
+			currentIndex = 0;
+		}
+	}
+
+	WayPointController wayPointController = new WayPointController();
+
+	public GameObject getCurrentWayPoint()
+	{
+		return wayPointController.objects[wayPointController.currentIndex];
+	}
+
+	void initialiseWayPointCheckList()
+	{
+		int _lengthOfWayPoints = wayPointController.objects.Length;
+		wayPointController.checkList = new bool[_lengthOfWayPoints];
+		int _index = 0;
+
+		foreach (GameObject objectByTagName in wayPointController.objects)
+		{
+			wayPointController.checkList[_index] = false;
+			_index++;
+		}
+
+	}
+
+	void setWayPointFlag(bool _hasReachedWayPoint)
+	{
+		wayPointController.checkList[wayPointController.currentIndex] = _hasReachedWayPoint;
+	}
+
+	void setNextSequencialWayPoint()
+	{
+
+		if (!(wayPointController.currentIndex >= wayPointController.checkList.Length - 1))
+		{
+
+			wayPointController.checkList[wayPointController.currentIndex] = false;
+			wayPointController.currentIndex++;
+
+		}
+
+	}
+
+	bool allWayPointsVisited()
+	{
+		int _lengthOfWayPoints = wayPointController.objects.Length;
+		int _index = 0;
+		int _visitedLength = 0;
+
+		foreach (GameObject objectByTagName in wayPointController.objects)
+		{
+			if (wayPointController.checkList[_index] == true)
+			{
+				_visitedLength++;
+			}
+			_index++;
+		}
+
+		bool _allVisited = (_visitedLength == wayPointController.objects.Length);
+
+		return _allVisited;
+
+	}
+
+	bool wayPointReached(GameObject _target)
+	{
+
+		bool _wayPointReached = false;
+
+		float distance = (transform.position - _target.transform.position).sqrMagnitude;
+
+		if (distance <= 0.001)
+		{
+			_wayPointReached = true;
+		}
+
+		return _wayPointReached;
+
+	}
+
+	void Start()
+	{
+		wayPointController.tagName = tagName;
+		wayPointController.objects = GameObject.FindGameObjectsWithTag(wayPointController.tagName);
+		initialiseWayPointCheckList();
+	}
+
+	void Update()
+	{
+
+		if (wayPointController.checkList[wayPointController.currentIndex] == false)
+		{
+
+			GameObject _target = wayPointController.objects[wayPointController.currentIndex]; // SHOULD FIND THE CLOSEST.
+
+			bool _hasReachedWayPoint = wayPointReached(_target);
+
+			if (_hasReachedWayPoint == true)
+			{
+				setWayPointFlag(_hasReachedWayPoint);
+				setNextSequencialWayPoint();
+			}
+
+		}
+
+	}
+
+}
